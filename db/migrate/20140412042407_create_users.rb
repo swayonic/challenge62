4e87bbd34e96c8a03e8f054c4c54f9dc06b4ba69
HermanKwok <herman.wy.kwok@gmail.com>
2014-04-12 12:45:44 +0800

    user table defined

--- db/migrate/20140412042407_create_users.rb
+++ db/migrate/20140412042407_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.integer :users_id
+      t.string :name
+      t.string :email
+      t.string :locale
       t.timestamps
     end
   end
