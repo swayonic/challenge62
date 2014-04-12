fc3f5e1527addc0355755e55d531da3269e47163
Swayonic <swayonic@gmail.com>
2014-04-12 15:15:13 +0800

    Wrong syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_access
 
   def set_access
-    @response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Origin"] = "*"
   end
 
 end
